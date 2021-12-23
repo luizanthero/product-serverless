b7e9907469f53dfcf7e5888eefa513a4bc8927e4
Luiz Anthero Gama <luizanthero@icloud.com>
2021-12-23 12:09:20 -0300

    add product factory methods

---
+++ src/configs/utils/product-factory.util.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// eslint-disable-next-line node/no-unpublished-require
+const faker = require('faker')
+
+const product = {
+  factory(status) {
+    return {
+      id: faker.datatype.number(10000),
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price(),
+      status: status || faker.datatype.boolean(),
+      image: faker.image,
+      amount: faker.datatype.number(100)
+    }
+  },
+  factoryError(status) {
+    return {
+      id: faker.datatype.number(10000),
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: faker.commerce.price(),
+      status: status || faker.datatype.boolean(),
+      amount: faker.datatype.number(100)
+    }
+  }
+}
+
+module.exports = product

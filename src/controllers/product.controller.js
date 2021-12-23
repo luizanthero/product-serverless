505a55f627f3d6d70972481de5fd28e91fd52063
Luiz Anthero Gama <luizanthero@icloud.com>
2021-12-23 08:44:47 -0300

    add validate body payload

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,9 +2,16 @@
 
 const service = require('../services/product.service')
 
+const validator = require('../configs/common/validator.service')
+const mandatoryFields = require('../configs/utils/mandatoryFields.util')
+
+const productFields = mandatoryFields.product
+
 const controller = {
   post(payload) {
-    service.post(payload)
+    const { validateStatus, validateMessage } = validator.validateBodyPayload(payload, productFields)
+
+    validateStatus ? service.post(payload) : service.delete({ ...payload, errorMessage: validateMessage })
   }
 }
 

a8ad55ca122a1f35c01cea44ec508547e044cdf2
Luiz Anthero Gama <luizanthero@icloud.com>
2021-12-23 14:58:35 -0300

    add lambda executation

--- src/lambdas/read-file.js
+++ src/lambdas/read-file.js
@@ -1,5 +1,21 @@
 'use strict'
 
-const handler = async (event) => {}
+const controller = require('../controllers/read-file.controller')
+
+const handler = async (event) => {
+  try {
+    const { bucket, object } = event.Records[0].s3
+
+    console.info('Starting Execution...')
+
+    await controller.getS3File(bucket.name, object.key, process.env.PRODUCT_QUEUE_URL)
+
+    console.info('Execution finished!!!')
+
+    return true
+  } catch (error) {
+    throw Error(error.message)
+  }
+}
 
 module.exports = { handler }
